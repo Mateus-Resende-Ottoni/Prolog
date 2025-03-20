108dddbfbc124b5e3234da1b4855c376d09743a0
Mateus Ottoni <mateus.resende.ottoni@gmail.com>
2025-03-20 14:53:14 -0300

    Update dungeonsanddraongs.pl
    - Fim e correção da lista de armas
    - Especificação e correção da fórmula de cálculo de vida

--- Dungeons&Dragons_Ficha/dungeonsanddragons.pl
+++ Dungeons&Dragons_Ficha/dungeonsanddragons.pl
@@ -196,7 +196,8 @@ hit_points(HP_total) :-
   character_class(Class),
   class_hp(Class, Class_HP),
   attribute_modifier(constitution, Mod),
-  HP_total is Level * (Class_HP + Mod).
+  HP_total is (Level * (Class_HP + Mod)) + Class_HP.
+% Valores levam em contam o HP padrão ganho por nível
 class_hp(barbarian, 7).
 class_hp(bard,      5).
 class_hp(cleric,    5).
@@ -215,29 +216,35 @@ class_hp(wizard,    4).
 weapon_stats(club,            4,  strength).
 weapon_stats(dagger,          4, dexterity).
 weapon_stats(greatclub,       8,  strength).
-
+weapon_stats(handaxe,         6,  strength).
+weapon_stats(javelin,         6,  strength).
+weapon_stats(light_hammer,    4,  strength).
+weapon_stats(mace,            6,  strength).
+weapon_stats(quarterstaff,    6,  strength).
+weapon_stats(sickle,          4,  strength).
+weapon_stats(spear,           6,  strength).
+weapon_stats(unarmed,         1,  strength).
 weapon_stats(light_crossbow,  8, dexterity).
 weapon_stats(dart,            4, dexterity).
 weapon_stats(shortbow,        6, dexterity).
 weapon_stats(sling,           4, dexterity).
-
-weapon_stats(battleaxe,       8, dexterity).
-weapon_stats(flail,           8, dexterity).
-weapon_stats(glaive,         10, dexterity).
-weapon_stats(greataxe,       12, dexterity).
-weapon_stats(greatsword,     12, dexterity).
-weapon_stats(halberd,        10, dexterity).
-weapon_stats(lance,          12, dexterity).
-weapon_stats(longsword,       8, dexterity).
-weapon_stats(maul,           12, dexterity).
-weapon_stats(morningstar,     8, dexterity).
-weapon_stats(pike,           10, dexterity).
+weapon_stats(battleaxe,       8,  strength).
+weapon_stats(flail,           8,  strength).
+weapon_stats(glaive,         10,  strength).
+weapon_stats(greataxe,       12,  strength).
+weapon_stats(greatsword,     12,  strength).
+weapon_stats(halberd,        10,  strength).
+weapon_stats(lance,          12,  strength).
+weapon_stats(longsword,       8,  strength).
+weapon_stats(maul,           12,  strength).
+weapon_stats(morningstar,     8,  strength).
+weapon_stats(pike,           10,  strength).
 weapon_stats(rapier,          8, dexterity).
 weapon_stats(scimitar,        6, dexterity).
 weapon_stats(shortsword,      6, dexterity).
 weapon_stats(trident,         6, dexterity).
-weapon_stats(war_pick,        8, dexterity).
-weapon_stats(warhammer,       8, dexterity).
+weapon_stats(war_pick,        8,  strength).
+weapon_stats(warhammer,       8,  strength).
 weapon_stats(whip,            4, dexterity).
 weapon_stats(blowgun,         1, dexterity).
 weapon_stats(hand_crossbow,   6, dexterity).
@@ -248,7 +255,7 @@ weapon_proficiency(Weapon_type, Proficiency_Bonus) :-
   item_proficiency(Weapon_type) , !,
   character_level(Level),
   proficiency_modifier(Level, Proficiency_Bonus) .
-weapon_proficiency(Weapon_type, Proficiency_Bonus) :-
+weapon_proficiency(_, Proficiency_Bonus) :-
   Proficiency_Bonus is 0.
 
 % Testes de ataque
