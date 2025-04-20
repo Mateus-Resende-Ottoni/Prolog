b2a025197d621e1ca6a25cede422ba85a1cd91c7
Mateus Ottoni <mateus.resende.ottoni@gmail.com>
2025-04-20 12:44:59 -0300

    Update Feiticeiros&Maldicoes_Ficha
    - Adição da lista de atributos por perícia
    - Adição do cálculo de modificador de atributo

--- Feiticeiros&Maldicoes_Ficha/atributos.pl
+++ Feiticeiros&Maldicoes_Ficha/atributos.pl
@@ -28,14 +28,36 @@ personagem_ca_bonus(0).
 
 %%%%%%%%%% Fatos e cálculos
 
-% Total em atributos
+%%% Total em atributos
 personagem_atributo(Atributo, Total) :-
     personagem_atributo_base(Atributo, X),
     personagem_atributo_bonus(Atributo, Y),
     Total is X + Y.
 
+%%% Modificar de atributo
+personagem_modificador(Atributo, Modificador) :-
+    personagem_atributo(Atributo, Total),
+    modificador(Total, Modificador).
+% Tabela dos modificadores
+modificador(Valor, -5) :- Valor ==  1 , !.
+modificador(Valor, -4) :- Valor >=  2 , Valor =<  3 ,!.
+modificador(Valor, -3) :- Valor >=  4 , Valor =<  5 ,!.
+modificador(Valor, -2) :- Valor >=  6 , Valor =<  7 ,!.
+modificador(Valor, -1) :- Valor >=  8 , Valor =<  9 ,!.
+modificador(Valor,  0) :- Valor >= 10 , Valor =< 11 ,!.
+modificador(Valor,  1) :- Valor >= 12 , Valor =< 13 ,!.
+modificador(Valor,  2) :- Valor >= 14 , Valor =< 15 ,!.
+modificador(Valor,  3) :- Valor >= 16 , Valor =< 17 ,!.
+modificador(Valor,  4) :- Valor >= 18 , Valor =< 19 ,!.
+modificador(Valor,  5) :- Valor >= 20 , Valor =< 21 ,!.
+modificador(Valor,  6) :- Valor >= 22 , Valor =< 23 ,!.
+modificador(Valor,  7) :- Valor >= 24 , Valor =< 25 ,!.
+modificador(Valor,  8) :- Valor >= 26 , Valor =< 27 ,!.
+modificador(Valor,  9) :- Valor >= 28 , Valor =< 29 ,!.
+modificador(Valor, 10) :- Valor >= 30.
 
-% Pontos de vida total
+
+%%% Pontos de vida total
 pontos_de_vida(PV) :-
     personagem_pv_base(PV_base),
     personagem_pv_bonus(PV_bonus),
--- Feiticeiros&Maldicoes_Ficha/pericias.pl
+++ Feiticeiros&Maldicoes_Ficha/pericias.pl
@@ -40,3 +40,49 @@ personagem_pericia(vontade,         proeficiente) :- !.
 personagem_pericia(_,               nao_proeficiente).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%% Lista de atributos das perícias
+pericia_atributo(acrobacia,       destreza).
+pericia_atributo(astucia,         inteligencia).
+pericia_atributo(atletismo,       forca).
+pericia_atributo(enganacao,       carisma).
+pericia_atributo(feiticaria,      inteligencia).
+pericia_atributo(fortitude,       constituicao).
+pericia_atributo(furtividade,     destreza).
+pericia_atributo(historia,        inteligencia).
+pericia_atributo(integridade,     constituicao).
+pericia_atributo(intimidacao,     carisma).
+pericia_atributo(intuicao,        sabedoria).
+pericia_atributo(investigacao,    inteligencia).
+
+% Luta é tanto de destreza quanto força
+pericia_atributo(luta,            destreza) :-
+    personagem_atributo(destreza, Destreza_Total),
+    personagem_atributo(forca,    Forca_Total),
+    Destreza_Total > Forca_Total, !.
+pericia_atributo(luta,            forca).
+
+pericia_atributo(medicina,        sabedoria).
+pericia_atributo(ocultismo,       sabedoria).
+pericia_atributo(percepcao,       sabedoria).
+pericia_atributo(performance,     carisma).
+pericia_atributo(persuasao,       carisma).
+
+% Pontaria é tanto de destreza quanto força
+pericia_atributo(pontaria,        destreza) :-
+    personagem_atributo(destreza, Destreza_Total),
+    personagem_atributo(forca,    Forca_Total),
+    Destreza_Total > Forca_Total, !.
+pericia_atributo(pontaria,        forca).
+
+pericia_atributo(prestidigitacao, destreza).
+pericia_atributo(reflexos,        destreza).
+pericia_atributo(religiao,        inteligencia).
+pericia_atributo(vontade,         sabedoria).
+
+% Complementares
+% pericia_atributo(direcao,         inteligencia) :- !.
+% pericia_atributo(sobrevivencia,   sabedoria) :- !.
+% pericia_atributo(tecnologia,      inteligencia) :- !.
