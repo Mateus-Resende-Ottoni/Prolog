e1023da3a63ab24c40e1ddff469e388e44d1706e
Mateus Ottoni <mateus.resende.ottoni@gmail.com>
2025-04-21 13:02:08 -0300

    Update Feiticeiros&Maldicoes_Ficha
    - Adição de 'aptidoes.pl' (Lista de aptidões ainda incompleta)

---
+++ Feiticeiros&Maldicoes_Ficha/aptidoes.pl
@@ -0,0 +1,48 @@
+%%%%%% Ficha de Customização %%%%%%
+
+%%% Níveis de aptidões
+personagem_aptidao_nivel(energia,            0).
+personagem_aptidao_nivel(controle_e_leitura, 0).
+personagem_aptidao_nivel(barreira,           0).
+personagem_aptidao_nivel(dominio,            0).
+personagem_aptidao_nivel(energia_reversa,    0).
+
+%%% Lista de aptidões do personagem
+personagem_aptidao(aura_reforcada).
+personagem_aptidao(aura_anuladora).
+personagem_aptidao(rastreio_avancado).
+personagem_aptidao(tecnicas_de_barreira).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%% Lista de aptidões amaldiçoadas
+%%% aptidao(Nome_da_aptidao, Tipo_da_aptidao)
+% Aptidões de aura
+aptidao(aura_elemental, aura).
+aptidao(aura_elemental_aprimorada, aura).
+aptidao(aura_reforcada, aura).
+aptidao(aura_impenetravel, aura).
+aptidao(casulo_de_energia, aura).
+aptidao(aura_inofensiva, aura).
+aptidao(aura_controlada, aura).
+aptidao(aura_do_comandante, aura).
+aptidao(aura_macabra, aura).
+aptidao(aura_lacerante, aura).
+aptidao(aura_macica, aura).
+aptidao(aura_chamativa, aura).
+aptidao(aura_anuladora, aura).
+aptidao(afinidade_ampliada, aura).
+aptidao(absorcao_elemental, aura).
+aptidao(aura_de_contencao, aura).
+aptidao(aura_drenadora, aura).
+aptidao(aura_embacada, aura).
+aptidao(aura_do_bastiao, aura).
+aptidao(aura_movedica, aura).
+aptidao(enganacao_projetada, aura).
+aptidao(transferencia_de_aura, aura).
+aptidao(aura_redirecionada, aura).
+aptidao(concentrar_aura, aura).
+aptidao(golpe_com_aura, aura).
+aptidao(aura_excessiva, aura).
+
