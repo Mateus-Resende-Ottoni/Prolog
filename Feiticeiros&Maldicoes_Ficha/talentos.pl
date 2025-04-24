362b9a62453ca7a35eb022424ccaed1e0b543674
Mateus Ottoni <mateus.resende.ottoni@gmail.com>
2025-04-24 16:00:11 -0300

    Update Feiticeiros&Maldicoes_Ficha
    - Adição de 'talentos.pl' e lista de talentos

---
+++ Feiticeiros&Maldicoes_Ficha/talentos.pl
@@ -0,0 +1,74 @@
+%%%%%% Ficha de Customização %%%%%%
+
+%%% Lista de talentos do personagem
+personagem_talento(ataque_infalivel).
+personagem_talento(incremento_de_atributo, inteligencia).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%% Lista de Talentos
+%%% Talentos Gerais
+talento(adepto_de_combate).
+talento(afinidade_com_tecnica).
+talento(arremessos_debilitantes).
+talento(artesao_amaldicoado).
+talento(ataque_infalivel).
+talento(atencao_infalivel).
+talento(atirador_preciso).
+talento(dedicacao_recompensadora).
+talento(favorecido_pela_sorte).
+talento(feiticeiro_companheiro).
+talento(guarda_infalivel).
+talento(incremento_de_atributo).
+talento(investida_aprimorada).
+talento(maldicao_de_bolso).
+talento(mestre_das_armas).
+talento(mestre_defensivo).
+talento(provocacao_desafiadora).
+talento(resiliencia_melhorada).
+talento(tecnicas_agressivas_de_escudo).
+talento(tecnicas_de_arremesso).
+talento(tecnicas_de_reacao_rapida).
+talento(tecnicas_defensivas_de_escudo).
+talento(tempestade_de_ideias).
+talento(tecnicas_de_empunhadura_dupla).
+talento(tecnicas_de_imobilizacao).
+talento(alma_inquebravel).
+talento(robustez_aprimorada).
+talento(determinado_a_viver).
+talento(correr_e_atirar).
+talento(tecnicas_de_esquiva).
+talento(tecnicas_de_mobilidade).
+talento(discurso_motivador).
+talento(tecnicas_de_ocultamento).
+talento(aptidao_desenvolvida).
+talento(disparos_perfurantes).
+talento(mestre_da_criacao).
+talento(mestre_dos_chicotes).
+talento(tecnicas_do_sentinela).
+talento(rapido_no_gatilho).
+talento(especialista_em_concussao).
+talento(especialista_em_cortes).
+talento(especialista_em_perfuracao).
+talento(mestre_do_arremesso).
+talento(segredos_do_artesao_da_alma).
+
+%%% Talentos exclusivos a origens
+talento(familiaridade_com_tecnica).
+talento(ideias_originais).
+talento(conhecimentos_sigilosos).
+talento(manual_de_tecnica).
+talento(expansao_de_reserva).
+talento(quebra_de_limites).
+talento(desarme_oportunista).
+talento(regeneracao_maior).
+talento(fisico_aperfeicoado).
+talento(reposicao_sanguinea).
+talento(coleta_de_talismas).
+talento(estudo_amaldicoado).
+talento(expansao_de_estilo).
+talento(nocao_e_preparacao).
+talento(nucleos_especializados).
+talento(nucleos_reforcados).
+
+
