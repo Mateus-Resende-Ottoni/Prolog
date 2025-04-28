e14363ca793eb6e7f211ab4b4cb362472a25fa18
Mateus Ottoni <mateus.resende.ottoni@gmail.com>
2025-04-28 19:29:17 -0300

    Update Feiticeiros&Maldicoes_Ficha
    - Adição da lista de pré-requisitos de talentos

--- Feiticeiros&Maldicoes_Ficha/talentos.pl
+++ Feiticeiros&Maldicoes_Ficha/talentos.pl
@@ -54,6 +54,8 @@ talento(mestre_do_arremesso).
 talento(segredos_do_artesao_da_alma).
 
 %%% Talentos exclusivos a origens
+%%% (Restrição estabelecida na
+%%%   lista de pré-requisitos)
 talento(familiaridade_com_tecnica).
 talento(ideias_originais).
 talento(conhecimentos_sigilosos).
@@ -72,3 +74,97 @@ talento(nucleos_especializados).
 talento(nucleos_reforcados).
 
 
+%%%%% Lista de pré-requisitos de Talentos
+%%% Talentos gerais
+talento_requisito(tecnicas_de_empunhadura_dupla) :- !,
+    personagem_atributo(forca, A1), personagem_atributo(destreza, A2),
+    (A1 >= 14; A2 >= 14).
+talento_requisito(tecnicas_de_imobilizacao) :- !,
+    personagem_atributo(forca, A1), personagem_atributo(constituicao, A2),
+    (A1 >= 16; A2 >= 16).
+talento_requisito(alma_inquebravel) :- !,
+    personagem_atributo(constituicao, A), A >= 14.
+talento_requisito(robustez_aprimorada) :- !,
+    personagem_atributo(constituicao, A), A >= 14.
+talento_requisito(determinado_a_viver) :- !,
+    personagem_atributo(constituicao, A), personagem_nivel(N),
+    (A >= 16; N >= 10).
+talento_requisito(correr_e_atirar) :- !,
+    personagem_atributo(destreza, A), A >= 14.
+talento_requisito(tecnicas_de_esquiva) :- !,
+    personagem_atributo(destreza, A), A >= 14.
+talento_requisito(tecnicas_de_mobilidade) :- !,
+    personagem_atributo(destreza, A), A >= 14.
+% Discurso Motivador
+talento_requisito(tecnicas_de_ocultamento) :- !,
+    personagem_pericia(furtividade, P), (P = proficiente; P = especialista).
+talento_requisito(aptidao_desenvolvida) :- !,
+    personagem_nivel(N), N >= 4.
+talento_requisito(disparos_perfurantes) :- !,
+    personagem_nivel(N), N >= 4.
+% Mestre da Criação
+talento_requisito(mestre_dos_chicotes) :- !,
+    personagem_nivel(N), N >= 5.
+talento_requisito(tecnicas_do_sentinela) :- !,
+    personagem_nivel(N), N >= 5.
+% Rápido no Gatilho
+talento_requisito(especialista_em_concussao) :- !,
+    personagem_nivel(N), N >= 8.
+talento_requisito(especialista_em_cortes) :- !,
+    personagem_nivel(N), N >= 8.
+talento_requisito(especialista_em_perfuracao) :- !,
+    personagem_nivel(N), N >= 8.
+% Mestre do Arremesso
+% Segredos do Artesão da Alma
+
+%%% Talentos de Origem
+talento_requisito(familiaridade_com_tecnica) :- !,
+    personagem_origem(inato),
+    personagem_nivel(N), N >= 12.
+talento_requisito(ideias_originais) :- !,
+    personagem_origem(inato),
+    personagem_nivel(N), N >= 5.
+talento_requisito(conhecimentos_sigilosos) :- !,
+    personagem_origem(herdado),
+    personagem_nivel(N), N >= 6.
+talento_requisito(manual_de_tecnica) :- !,
+    personagem_origem(herdado),
+    personagem_nivel(N), N >= 5.
+talento_requisito(expansao_de_reserva) :- !,
+    personagem_origem(derivado),
+    personagem_nivel(N), N >= 8.
+talento_requisito(quebra_de_limites) :- !,
+    personagem_origem(derivado),
+    personagem_nivel(N), N >= 6.
+talento_requisito(desarme_oportunista) :- !,
+    personagem_origem(restringido),
+    personagem_nivel(N), N >= 4.
+talento_requisito(regeneracao_maior) :- !,
+    personagem_origem(restringido),
+    personagem_nivel(N), N >= 10.
+talento_requisito(fisico_aperfeicoado) :- !,
+    personagem_origem(feto_amaldicoado_hibrido),
+    personagem_nivel(N), N >= 6.
+talento_requisito(reposicao_sanguinea) :- !,
+    personagem_origem(feto_amaldicoado_hibrido),
+    personagem_nivel(N), N >= 6.
+talento_requisito(coleta_de_talismas) :- !,
+    personagem_origem(sem_tecnica).
+talento_requisito(estudo_amaldicoado) :- !,
+    personagem_origem(sem_tecnica).
+talento_requisito(expansao_de_estilo) :- !,
+    personagem_origem(sem_tecnica),
+    personagem_aptidao(dominio_simples),
+    personagem_nivel(N), N >= 6.
+talento_requisito(nocao_e_preparacao) :- !,
+    personagem_origem(sem_tecnica),
+    personagem_nivel(N), N >= 4.
+talento_requisito(nucleos_especializados) :- !,
+    personagem_origem(corpo_amaldicoado),
+    personagem_nivel(N), N >= 10.
+talento_requisito(nucleos_reforcados) :- !,
+    personagem_origem(corpo_amaldicoado),
+    personagem_nivel(N), N >= 10.
+
+% Valor padrão
+talento_requisito(_) :- true.
